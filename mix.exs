82739fa54075d7b15e60b6517e6d80b62b7145ae
Donald Lobo <lobo@Donalds-MBP.hsd1.ca.comcast.net>
2020-05-07 12:17:41 -0700

    Initial commit of issues elixir project

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule Issues.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :issues,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]],
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:credo, "~> 0.8.8", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.18", only: [:dev], runtime: false},
+      {:inch_ex, "~> 0.5", only: [:dev, :test], runtime: false},
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
